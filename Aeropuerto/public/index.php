987d1f31da3b8ab6a92a29d55f075024336ea3de
Gabrieldrc <gabriel.drc25.2@gmail.com>
2020-03-12 18:14:31 -0300

    Se agregó las primeras dos paginas, pero faltan terminar, y seguir añadiendo paginas y templates

--- Aeropuerto/public/index.php
+++ Aeropuerto/public/index.php
@@ -10,7 +10,9 @@ session_start();
 if (PHP_SAPI == 'cli-server') {
     $url  = parse_url($_SERVER['REQUEST_URI']);
     $file = __DIR__ . $url['path'];
-    if (is_file($file)) return false;
+    if (is_file($file)) {
+        return false;
+    }
 }
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
@@ -18,10 +20,29 @@ $twig = new \Twig\Environment($loader, [
     'cache' => false,
 ]);
 
+$conn = new \MongoDB\Client("mongodb://localhost");
+$vuelosService = new \Src\Servicios\VuelosServicio($conn->vuelosServicios->aviones);
+$registroDeVuelosServicio = new \Src\Servicios\VuelosServicio($conn->registroDeVuelosServicios->registroDeVuelos);
+
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$app->get('/home', function (Request $request, Response $response, $args) use ($twig) {
+    $template = $twig->load('index.html');
+    $response->getBody()->write(
+        $template->render(['Titulo' => 'Gabriel Airlines'])
+    );
+    return $response;
+});
+
+$app->get('/registro', function (Request $request, Response $response, $args) use ($twig, $vuelosService, $registroDeVuelosServicio) {
+    $template = $twig->load('index.html');
+    // $listaVuelos = $registroDeVuelosServicio
+    $response->getBody()->write(
+        $template->render([
+            'Titulo' => 'Gabriel Airlines',
+            'idVuelo' => 
+            ])
+    );
     return $response;
 });
 
